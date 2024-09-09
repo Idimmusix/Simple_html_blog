5e03c820603d2c4b0d0bd70b8b07a1029cf9afe9
Deji Adegbite <d2alphame@yahoo.com>
2024-09-09 18:26:32 -0400

    Setup a simple blog for idimma.com

---
+++ script.js
@@ -0,0 +1,5 @@
+// This script will handle expandable menu and other interactivity
+document.getElementById('home-link').addEventListener('click', function() {
+    window.location.href = 'https://idimma.com';
+  });
+  
